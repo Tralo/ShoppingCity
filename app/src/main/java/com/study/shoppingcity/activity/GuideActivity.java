dd73ee28df802488008781cc61cbf77895431d9a
Tralo <13580548169@163.com>
2016-11-13 23:19:29 +0800

    添加监听树来监听加载完后iv_red的左边距

--- app/src/main/java/com/study/shoppingcity/activity/GuideActivity.java
+++ app/src/main/java/com/study/shoppingcity/activity/GuideActivity.java
@@ -5,8 +5,10 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,11 +24,16 @@ public class GuideActivity extends AppCompatActivity {
     private Button btn_start;
     private ViewPager viewPager;
     private LinearLayout ll_point_group;
+    private ImageView iv_red;
     private int[] ids ={
             R.mipmap.guide_1,
             R.mipmap.guide_2,
             R.mipmap.guide_3
     };
+    //两点的间距
+    private int leftmax = 0s;
+
+    private int currPos = 0;
 
 
     private List<ImageView> imageViews = new ArrayList<ImageView>();
@@ -54,34 +61,41 @@ public class GuideActivity extends AppCompatActivity {
             LinearLayout.LayoutParams params
                     = new LinearLayout.LayoutParams(DensityUtil.dip2px(GuideActivity.this,10)
                     ,DensityUtil.dip2px(GuideActivity.this,10));
-            params.setMargins(DensityUtil.dip2px(GuideActivity.this,6),0,DensityUtil.dip2px(GuideActivity.this,6),0);
+            if(i != 0){
+                params.leftMargin =  DensityUtil.dip2px(GuideActivity.this,10);
+            }
+
+
             point.setLayoutParams(params);
             //添加到线性布里面
             ll_point_group.addView(point);
 
         }
-        dotViews.get(0).setBackgroundResource(R.drawable.point_red);
     }
 
     //设置viewpager的适配器,监听事件
     private void initViewPager() {
         viewPager.setAdapter(new GuidePagerAdapter());
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.i("GuideActivity","currPos: " + currPos +"    position:"+ position+"   positionOffset: " + positionOffset + "     positionOffsetPixels: " + positionOffsetPixels);
+                if(currPos > position){
+
+                }
+                if(currPos < position){
+
+                }
             }
 
             @Override
             public void onPageSelected(int position) {
+                currPos = position;
                 if(position == (ids.length -1 )){
                     btn_start.setVisibility(View.VISIBLE);
                 } else {
                     btn_start.setVisibility(View.GONE);
                 }
-                for(ImageView iv : dotViews){
-                    iv.setBackgroundResource(R.drawable.point_normal);
-                }
-                dotViews.get(position).setBackgroundResource(R.drawable.point_red);
             }
 
             @Override
@@ -89,6 +103,10 @@ public class GuideActivity extends AppCompatActivity {
 
             }
         });
+
+        //根据View的生命周期，当视图执行到onLayout或者onDraw的时候，视图的高和宽，边距都有类
+        iv_red.getViewTreeObserver().addOnGlobalLayoutListener(new MyOnGlobalLayoutListener());
+
 
     }
 
@@ -104,6 +122,7 @@ public class GuideActivity extends AppCompatActivity {
         btn_start = (Button) findViewById(R.id.btn_start);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         ll_point_group = (LinearLayout) findViewById(R.id.ll_point_group);
+        iv_red = (ImageView) findViewById(R.id.iv_red);
     }
 
 
@@ -130,6 +149,18 @@ public class GuideActivity extends AppCompatActivity {
         @Override
         public boolean isViewFromObject(View view, Object object) {
             return view == object;
+        }
+    }
+
+    class MyOnGlobalLayoutListener implements ViewTreeObserver.OnGlobalLayoutListener{
+
+        @Override
+        public void onGlobalLayout() {
+            //执行不止一次
+            iv_red.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+
+            leftmax = ll_point_group.getChildAt(1).getLeft() - ll_point_group.getChildAt(0).getLeft();
+
         }
     }
 
