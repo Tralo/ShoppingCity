f2fdfa0d800ecd6f5f90f6fd66be81e96378ae73
Tralo <13580548169@163.com>
2016-11-14 22:34:11 +0800

    RadioGroup加入到主页面

--- app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
+++ app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
@@ -2,31 +2,35 @@ package com.study.shoppingcity.fragment;
 
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.study.shoppingcity.R;
 import com.study.shoppingcity.base.BaseFragment;
 
 /**
  * Created by adventurer on 16-11-14.
  */
 public class ContentFragment extends BaseFragment {
-    private TextView tv;
+
+    private ViewPager viewPager;
+    private RadioGroup rg_main;
 
     @Override
     protected void initData() {
-        tv.setText("主界面");
-
+        //设置默认选中
+        rg_main.check(R.id.rb_home);
 
     }
 
     @Override
     public View initView() {
-        tv = new TextView(getContext());
-        tv.setTextSize(23);
-        tv.setGravity(Gravity.CENTER);
-        tv.setTextColor(Color.RED);
-        return tv;
+        View view = View.inflate(activity, R.layout.content_fragment,null);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        rg_main = (RadioGroup) view.findViewById(R.id.rg_main);
+        return view;
     }
 }
