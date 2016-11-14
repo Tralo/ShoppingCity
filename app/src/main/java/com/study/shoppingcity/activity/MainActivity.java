e0a4fd6c53561c8489005e25b756b786122c4211
Tralo <13580548169@163.com>
2016-11-14 22:06:18 +0800

    用Fragment替换左侧和主页的内容

--- app/src/main/java/com/study/shoppingcity/activity/MainActivity.java
+++ app/src/main/java/com/study/shoppingcity/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.study.shoppingcity.activity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,14 @@ import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.study.shoppingcity.R;
+import com.study.shoppingcity.fragment.ContentFragment;
+import com.study.shoppingcity.fragment.LeftMmenuFragment;
 import com.study.shoppingcity.utils.DensityUtil;
 
 public class MainActivity extends SlidingFragmentActivity {
+
+    private static final String MAIN_CONTENT_TAG = "main_content_tag";
+    private static final String LEFTMENU_TAG = "leftmenu_tag";
 
 
 
@@ -18,15 +25,36 @@ public class MainActivity extends SlidingFragmentActivity {
         super.onCreate(savedInstanceState);
         //1.设置主页面
         setContentView(R.layout.activity_main);
+        initSlideMenu();
+        initFragment();
+
+
+    }
+
+    private void initFragment() {
+        //1.得到FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+        //2.开启事务
+        FragmentTransaction ft = fm.beginTransaction();
+        //3.替换
+        ft.replace(R.id.fl_main_content,new ContentFragment(),MAIN_CONTENT_TAG);
+        ft.replace(R.id.fl_leftmenu,new LeftMmenuFragment(),LEFTMENU_TAG);
+        //4.提交
+        ft.commit();
+
+
+    }
+
+    private void initSlideMenu() {
         //2.设置左侧菜单
         setBehindContentView(R.layout.activity_leftmenu);
 
         //3.设置右侧菜单
         SlidingMenu slidingMenu = getSlidingMenu();
-        slidingMenu.setSecondaryMenu(R.layout.activit_rightmenu);
+//        slidingMenu.setSecondaryMenu(R.layout.activit_rightmenu);
 
         //4.设置模式:左侧菜单+ 主页， 左侧菜单+主页+右侧菜单，主页+右侧菜单
-        slidingMenu.setMode(SlidingMenu.LEFT_RIGHT);
+        slidingMenu.setMode(SlidingMenu.LEFT);
         //5.设置滑动模式:滑动边缘，全屏滑动，不可以滑动
 
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
--- app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
+++ app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
@@ -1,7 +1,10 @@
 package com.study.shoppingcity.fragment;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.study.shoppingcity.base.BaseFragment;
 
@@ -9,13 +12,21 @@ import com.study.shoppingcity.base.BaseFragment;
  * Created by adventurer on 16-11-14.
  */
 public class ContentFragment extends BaseFragment {
+    private TextView tv;
+
     @Override
     protected void initData() {
+        tv.setText("主界面");
+
 
     }
 
     @Override
     public View initView() {
-        return null;
+        tv = new TextView(getContext());
+        tv.setTextSize(23);
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(Color.RED);
+        return tv;
     }
 }
