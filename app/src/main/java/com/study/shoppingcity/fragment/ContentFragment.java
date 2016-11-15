9f13aec1a299a37ae669fc2ee8fb9cac4695b749
Tralo <13580548169@163.com>
2016-11-15 23:04:11 +0800

    viewpager加入basepager的各个子类

--- app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
+++ app/src/main/java/com/study/shoppingcity/fragment/ContentFragment.java
@@ -2,9 +2,11 @@ package com.study.shoppingcity.fragment;
 
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -33,6 +35,8 @@ public class ContentFragment extends BaseFragment {
     @ViewInject(R.id.rg_main)
     public RadioGroup rg_main;
 
+    private int[] rbIds = {R.id.rb_home,R.id.rb_newscenter,R.id.rb_smartservice,R.id.rb_govaffair,R.id.rb_setting};
+
 
     private List<BasePager> pagers = new ArrayList<BasePager>();
 
@@ -48,7 +52,25 @@ public class ContentFragment extends BaseFragment {
 
         //设置默认选中
         rg_main.check(R.id.rb_home);
+        //设置ViewPager的适配器
 
+        viewPager.setAdapter(new ContentFragmentAdapter());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                rg_main.check(rbIds[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
@@ -57,4 +79,34 @@ public class ContentFragment extends BaseFragment {
         x.view().inject(ContentFragment.this,view);
         return view;
     }
+
+    class ContentFragmentAdapter extends PagerAdapter{
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            BasePager basePager = pagers.get(position);
+            View rootView = basePager.rootView;
+            container.addView(rootView);
+
+            return rootView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            BasePager basePager = pagers.get(position);
+            container.removeView(basePager.rootView);
+        }
+
+        @Override
+        public int getCount() {
+            return pagers.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+    }
+
+
 }
