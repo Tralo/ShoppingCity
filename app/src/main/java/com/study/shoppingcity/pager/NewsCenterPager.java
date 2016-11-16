0d790560cc5ee9dd5ec88ab96a57799b4e9dc1e1
Tralo <13580548169@163.com>
2016-11-17 00:01:41 +0800

    使用xUtils3请求网络数据

--- app/src/main/java/com/study/shoppingcity/pager/NewsCenterPager.java
+++ app/src/main/java/com/study/shoppingcity/pager/NewsCenterPager.java
@@ -2,15 +2,23 @@ package com.study.shoppingcity.pager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
 import com.study.shoppingcity.base.BasePager;
+import com.study.shoppingcity.utils.Constants;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by adventurer on 16-11-15.
  */
 public class NewsCenterPager extends BasePager {
+
+    private static final  String TAG = "NewsCenterPager";
 
 
     public NewsCenterPager(Context context) {
@@ -28,5 +36,37 @@ public class NewsCenterPager extends BasePager {
         textView.setTextColor(Color.RED);
         fl_content.addView(textView);
         //绑定数据
+
+        getDataFromNet();
     }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(Constants.NEW_CENTER_PAGE_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.i(TAG,"联网成功获取的数据:  " + result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+                Log.i(TAG,"请求失败: " + ex.getMessage());
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+        
+    }
+
+
 }
